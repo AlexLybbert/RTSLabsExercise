6075313b72c8483e0228a7fea6d4297ee742ff4d
Alex Lybbert <xanderjpn@gmail.com>
2023-03-27 22:13:14 -0400

    Add project files.

---
+++ RTSLabsExercise/IntegerComparer.cs
@@ -0,0 +1,74 @@
+﻿namespace RTSLabsExercise
+{
+    public class IntegerComparer
+    {
+        //The longwinded version
+        public Dictionary<string, int> aboveBelow(List<int> numbers, int comparison)
+        {
+            //Initial setup
+            Dictionary<string, int> result = new Dictionary<string, int>();
+            int aboveCount = 0;
+            int belowCount = 0;
+
+            //Loop through numbers in the list
+            foreach (int number in numbers)
+            {
+                //Compare number against comparison value
+                if (number > comparison)
+                {
+                    //Increment the count of numbers above the comparison value
+                    aboveCount++;
+                }
+                else if (number < comparison)
+                {
+                    //Increment the count of numbers below the comparison value
+                    belowCount++;
+                }
+            }
+
+            //Add results to dictionary
+            result.Add("above", aboveCount);
+            result.Add("below", belowCount);
+
+            return result;
+        }
+
+        //Semi-concise version
+        public Dictionary<string, int> aboveBelowSemiConcise(List<int> numbers, int comparison)
+        {
+            //Initial setup
+            var counts = new Dictionary<string, int>() { { "above", 0 }, { "below", 0 } };
+
+            //Loop through numbers in the list
+            foreach (var number in numbers)
+            {
+                //Compare number against comparison value
+                if (number > comparison)
+                {
+                    //Increment the count of numbers above the comparison value
+                    counts["above"]++;
+                }
+                else if (number < comparison)
+                {
+                    //Increment the count of numbers below the comparison value
+                    counts["below"]++;
+                }
+            }
+
+            return counts;
+        }
+
+        //Concise version using LINQ
+        public Dictionary<string, int> aboveBelowStreamlined(List<int> numbers, int comparison)
+        {
+            //Create dictionary with the counts of the numbers above and below the comparison value
+            return new Dictionary<string, int>
+            {
+                ["above"] = numbers.Count(x => x > comparison),
+                ["below"] = numbers.Count(x => x < comparison)
+            };
+        }
+    }
+
+
+}
---
+++ RTSLabsExercise/StringRotator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTSLabsExercise
+{
+    public class StringRotator
+    {
+        //Long-winded version
+        public static string stringRotation(string original, int rotationAmount)
+        {
+            int length = original.Length;
+            rotationAmount = rotationAmount % length; // ensure rotationAmount is within the range of the string length
+            if (rotationAmount == 0) return original; // no rotation needed
+            return original.Substring(length - rotationAmount) + original.Substring(0, length - rotationAmount);
+        }
+
+        //Concise version
+        public static string stringRotationConcise(string original, int rotationAmount)
+        {
+            // Calculate the effective rotation amount within the range of the string length
+            rotationAmount %= original.Length;
+
+            // If no rotation is needed, return the original string
+            if (rotationAmount == 0)
+            {
+                return original;
+            }
+
+            // Select the rightmost substring of length 'rotationAmount' and concatenate it
+            // with the leftmost substring of length 'original.Length - rotationAmount'
+            // This creates the rotated string with the overflow characters at the beginning
+            return original[^rotationAmount..] + original[..^rotationAmount];
+        }
+
+    }
+}
